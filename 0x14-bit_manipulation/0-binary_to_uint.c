1ecce3c251c64b590f004abcfd112dc6486c42f6
Jesse Amarquaye <96346994+amarquaye@users.noreply.github.com>
2023-04-10 01:38:58 +0000

    Updated

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,29 +9,29 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int ui;
-	int len, base_two;
+	unsigned int uix;
+	int lens, base_two;
 
 	if (!b)
 		return (0);
 
-	ui = 0;
+	uix = 0;
 
-	for (len = 0; b[len] != '\0'; len++)
+	for (lens = 0; b[lens] != '\0'; lens++)
 		;
 
-	for (len--, base_two = 1; len >= 0; len--, base_two *= 2)
+	for (lens--, base_two = 1; lens >= 0; lens--, base_two *= 2)
 	{
-		if (b[len] != '0' && b[len] != '1')
+		if (b[lens] != '0' && b[lens] != '1')
 		{
 			return (0);
 		}
 
-		if (b[len] & 1)
+		if (b[lens] & 1)
 		{
-			ui += base_two;
+			uix += base_two;
 		}
 	}
 
-	return (ui);
+	return (uix);
 }
